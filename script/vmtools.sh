d58ed765b02102fc123dfd2612c460c70be6d86a
CorbanR <corban@raunco.co>
2016-11-03 22:24:48 -0600

    Cleanup

--- script/vmtools.sh
+++ script/vmtools.sh
@@ -8,7 +8,26 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     mkdir -p /mnt/cdrom
     mount -o loop /home/vagrant/linux.iso /mnt/cdrom
     tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-    /tmp/vmware-tools-distrib/vmware-install.pl -d
+
+    #See https://gist.github.com/lamw/bae9687faa14c7d7ee18
+    cat > /tmp/answer << __ANSWER__
+    yes
+    /usr/bin
+    /etc
+    /etc/init.d
+    /usr/sbin
+    /usr/lib/vmware-tools
+    yes
+    /usr/share/doc/vmware-tools
+    yes
+    yes
+    no
+    no
+    yes
+    no
+__ANSWER__
+
+    /tmp/vmware-tools-distrib/vmware-install.pl < /tmp/answer
     rm /home/vagrant/linux.iso
     umount /mnt/cdrom
     rmdir /mnt/cdrom
